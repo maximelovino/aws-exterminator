db5b9367ce0b0ca93860209bee89001b5a6e890b
Maxime Lovino <maximelovino@gmail.com>
2019-05-04 23:34:51 +0200

    Getting cloudwatch metrics now

--- exterminator.py
+++ exterminator.py
@@ -1,8 +1,8 @@
+import boto3
 from dotenv import load_dotenv
+from datetime import datetime, timedelta
 
 load_dotenv()
-
-import boto3
 
 ec2_client = boto3.client('ec2')
 
@@ -10,10 +10,24 @@ response = ec2_client.describe_regions()
 regions = list(map(lambda x: x['RegionName'], response['Regions']))
 
 regions_clients = list(map(lambda region: boto3.resource('ec2', region_name=region), regions))
+regions_cloudwatches = list(map(lambda region: boto3.client('cloudwatch', region_name=region), regions))
 
-
-for i,client in enumerate(regions_clients):
+for i, client in enumerate(regions_clients):
     print("Region: ", regions[i])
     instances = client.instances.all()
-    for i in instances:
-        print(i)
+    for instance in instances:
+        watch = regions_cloudwatches[i]
+        print(instance)
+        print("===========")
+        print('Metrics:')
+        metrics = watch.list_metrics(Namespace='AWS/EC2', Dimensions=[{"Name": "InstanceId",
+                                                                       "Value": instance.id}])
+        metrics = metrics['Metrics']
+        for met in metrics:
+            response = watch.get_metric_statistics(Namespace=met['Namespace'], MetricName=met['MetricName'], Dimensions=met['Dimensions'], Statistics=[
+                'SampleCount', 'Average', 'Sum', 'Minimum', 'Maximum',
+            ], Period=300, EndTime=datetime.utcnow(), StartTime=datetime.utcnow() - timedelta(seconds=600))
+
+            data = response['Datapoints'][0]
+            print(response['Label'], " => ", 'Average: ', data['Average'], ", Min: ", data['Minimum'], ', Max: ', data['Maximum'])
+        print("===========")
